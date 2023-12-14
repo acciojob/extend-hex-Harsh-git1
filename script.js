4193a4bbccaf8f905222eacce1e07a26fdb28a3b
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-14 14:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3, isCapital = false;
+    let output = "#";
+	
+    while(i < shortHex.length) {
+        let asciicode = shortHex[i].charCodeAt(0);
+        if(asciicode >= 65 && asciicode <= 90) {
+            isCapital = true;
+        }
+        output += shortHex[i] + shortHex[i];
+        i++;
+	}
+	return isCapital?output.toUpperCase():output;
+	
 };
 
+
+ 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
